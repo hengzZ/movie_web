7ffe3b52d926b19db3f6359335d0ea5df668fc8c
Wang <zhiheng.wang@intel.com>
2018-12-11 00:52:06 +0800

    views

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,24 @@
 # coding：utf8
 from . import admin
+from flask import render_template, redirect, url_for
 
 
 @admin.route("/")
 def index():
     return "<h1 style='color:red'>this is admin</h1>"
+
+# 登陆、退出
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+
+# 管理中心
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
